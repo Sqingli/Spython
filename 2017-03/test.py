688b73a740ada4241e894a394337568d98b97899
root <root@localhost.localdomain>
2017-03-28 17:47:15 +0800

    modify

--- 2017-03/test.py
+++ 2017-03/test.py
@@ -1,8 +1,13 @@
 
 from __future__ import division
+print
+print
+print
 for i in range(1,16):
-	for j in range(i,16):
-		print "%-2d/%-2d=%-8.3f" %(i, j, i / j),
+	print("%-13d" %(i)),
+for i in range(1,16):
+	for j in range(1, i):
+		print "%d/%-2d=%-8.3f" %(j, i, j / i),
 	print
 	print 
 	print 
